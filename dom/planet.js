4f317e41c87ec3d9b3960870dbbb7c2a208aed21
tatarudragos <tataru.dragos@yahoo.com>
2022-01-27 20:51:01 +0200

    main

--- dom/planet.js
+++ dom/planet.js
@@ -1,6 +1,36 @@
 var p = document.getElementById("green planet")
 p.innerText = "An alien appeared"
-p.style.color ="red"
+p.innerHTML = "<b>An alien appeared</b>"
 
+//putem seta stilul direct din js
+// dar recomandarea este sa folosim css+clase
+//p.style.color ="#64A4C5"
+//p.style.backgroundColor = "rgb(230,210,200)"
 
-document.getElementById("green planet").innerText = "An alien appeared" 
+//adauga o clasa elementului
+p.classList.add("warning")
+
+//pentru a sterge o clasa folosim remove in loc de add
+//p.classlist.remove("remove")
+
+var newElement = document.createElement("p")
+newElement.innerText = "Hello there!"
+//punem elementul in body la sfarsit
+//document.querySelector("body").appendChild(newElement)
+// punem elementul inaintea elementului p in body
+
+p.before(newElement)
+
+// sterge un element
+document.querySelector("#blueplanet").remove()
+// executa functia changeMessage cand se da click pe input
+document.querySelector("input").addEventListener("click", changeMessage)
+
+function changeMessage(){
+ document.querySelector("#redplanet").classList.add("warning")
+
+}
+// e.append(element)- adauga la sfarsitul elementului e
+// e.prepend (element)-adauga la inceputul elementului e
+//e.before (element)-adauga inaintea de elementul e
+//e.after (element)-adauga dupa elementul e
