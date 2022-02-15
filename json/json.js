62b1decb51f8605ada7bbdbae4e890de8b2d48a2
Tatarudragos <95440405+Tatarudragos@users.noreply.github.com>
2022-02-15 19:43:29 +0200

    commit on ''main''

---
+++ json/json.js
@@ -0,0 +1,17 @@
+//converse la json
+
+let hero = {
+    name: 'Superman'
+    power: '1000'
+    life: '100'
+}
+{
+    'heroes': [
+        {
+            'id': 1,
+            'name': 'Superman',
+            'power': 1000,
+            'life': 100
+        }
+    ]
+}
